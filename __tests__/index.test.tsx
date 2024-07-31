3a2a10706351ef8a478510fd17e1431d68f07704
Sherin <csherin111@gmail.com>
2024-08-01 00:09:37 +0400

    Initial commit

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,17 +1,29 @@
-/**
- * @jest-environment jsdom
- */
-import { render, screen } from "@testing-library/react";
-import Home from "@/pages/home/index";
-
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Home from "../app/Home";
+import Counter from "app/counter";
 describe("Home", () => {
   it("renders a heading", () => {
     render(<Home />);
 
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
-    });
+    const heading = screen.getByRole("heading", { level: 1 });
 
     expect(heading).toBeInTheDocument();
+  });
+});
+
+describe("Counter Component", () => {
+  test("renders initial count", () => {
+    render(<Counter />);
+    const countElement = screen.getByText("0");
+    expect(countElement).toBeInTheDocument();
+  });
+
+  test("increments count on button click", () => {
+    render(<Counter />);
+    const buttonElement = screen.getByRole("button", { name: "+" });
+    fireEvent.click(buttonElement);
+    const countElement = screen.getByText("1");
+    expect(countElement).toBeInTheDocument();
   });
 });
